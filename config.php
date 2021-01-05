790f4fb5dcb0cf305e8c3ad142eb8993f4225bbb
Francisco Jasso <pacojare_0223@hotmail.com>
2021-01-05 12:41:36 -0600

    Original

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+	session_start();
+
+    // connect to database
+    $conn = mysqli_connect("localhost", "root", "", "blogciencia");
+
+	if (!$conn) {
+		die("Error connecting to database: " . mysqli_connect_error());
+	}
+       
+	define ('ROOT_PATH', realpath(dirname(__FILE__)));
+	define('BASE_URL', 'http://localhost/blogciencia/');
+?>
---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+			<div class="logo_div">
+				<a href="index.php"><h1>LifeBlog</h1></a>
+			</div>
+			<ul>
+			  <li><a class="active" href="index.php">Inicio</a></li>
+			  <li><a href="#nuevo">Nuevos</a></li>
+			  <li><a href="#contacto">Contacto</a></li>
+			  <li><a href="#acercade">Acerca de</a></li>
+			</ul>
+		</div>
